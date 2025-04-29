49159c92c011ce28d0b4eccbd1aba1ce65cf66cd
GemelliAngelo <gemeangelo27@gmail.com>
2025-04-29 13:42:00 +0200

    add img views + fix scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./assets/css/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/games")
+      .then((res) => res.json())
+      .then((data) => setGames(data.data));
+  }, []);
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="container">
+      <div className="row">
+        {games &&
+          games.map((game) => (
+            <div key={game.id} className="col-4">
+              <div className="card">
+                <img
+                  src={`/img/${game.cover_url}`}
+                  alt={`cover-${game.title}`}
+                />
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
   );
 }
 
